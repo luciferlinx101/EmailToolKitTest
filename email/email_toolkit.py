24eac38055dca1b491b6e1417eddee68089c24cc
abhijeet <abhijeet@contlo.com>
2023-06-15 02:11:13 +0530

    Changing details

--- email/email_toolkit.py
+++ email/email_toolkit.py
@@ -3,16 +3,17 @@ from superagi.tools.base_tool import BaseToolKit, BaseTool
 from typing import Type, List
 from read_email import ReadEmailTool
 from send_email import SendEmailTool
+from send_email_attachment import SendEmailAttachmentTool
 
 from pydantic import BaseModel, Field
 
 
 class EmailToolKit(BaseToolKit, ABC):
-    name: str = "Email Toolkit"
-    description: str = "Changed Email Tool kit contains all tools related to sending email"
+    name: str = "Changed Email Toolkit"
+    description: str = "Email Tool kit contains all tools related to sending email"
 
     def get_tools(self) -> List[BaseTool]:
-        return [ReadEmailTool(), SendEmailTool()]
+        return [ReadEmailTool(), SendEmailTool(),SendEmailAttachmentTool()]
 
     def get_env_keys(self) -> List[str]:
         return ["EMAIL_ADDRESS", "EMAIL_PASSWORD", "EMAIL_SIGNATURE", "EMAIL_DRAFT_MODE_WITH_FOLDER", "EMAIL_SMTP_HOST", "EMAIL_SMTP_PORT","EMAIL_ATTACHMENT_BASE_PATH","EMAIL_IMAP_SERVER"]
